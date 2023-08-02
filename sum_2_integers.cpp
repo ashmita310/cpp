9fd783ff2ac999f33a60299c3a54772c923fb263
ashmita310 <ashmitasaha13@gmail.com>
2023-08-03 02:07:43 +0530

    sum of two integers

---
+++ sum_2_integers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 = 0,num2 = 0, sum = 0;
+    cout<<"Enter two numbers:"<<endl;
+    cin>>num1>>num2;
+    sum = num1 + num2;
+    cout<<"Sum:"<<sum<<endl;
+    return 0;
+    
+}
