18a5a80c79232e133a5373b69d8f6555e83b9c52
ashmita310 <ashmitasaha13@gmail.com>
2023-08-03 12:24:18 +0530

    area of rectangle

---
+++ rectangle_area.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int l=0,b=0,area=0;
+    cout<<"Enter the length and breadth:"<<endl;
+    cin>>l>>b;
+    area=l*b;
+    cout<<"Area of the rectangle is:"<<area<<endl;
+
+    return 0;
+}
