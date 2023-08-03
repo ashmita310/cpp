037728325f06e3819723d8e4b1d865fd3a83c72e
ashmita310 <ashmitasaha13@gmail.com>
2023-08-03 12:54:50 +0530

    perimeter of triangle

---
+++ perimeter_triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    float a=0.0,b=0.0,c=0.0,perimeter=0.0;
+    cout<<"Enter the sides of triangle:"<<endl;
+    cin>>a>>b>>c;
+    perimeter = a+b+c;
+    cout<<"Perimeter of triangle:"<<perimeter<<endl;
+
+    return 0;
+}
