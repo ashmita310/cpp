52b61ae39c41deadaab4e74e522b506612f4b502
ashmita310 <ashmitasaha13@gmail.com>
2023-08-05 01:12:13 +0530

    palindrome taking string

---
+++ palindromestr.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string input;
+    cout<<"Enter the string:"<<endl;
+    getline(cin,input);
+    bool ispalindrome=true;
+    int i;
+    int length=input.length();
+    for(i=0;i<length/2;i++)
+    {
+        if(input[i]!=input[length-i-1])
+        {
+          ispalindrome=false;
+          break;
+        }
+    }
+    if(ispalindrome)
+    cout<<"Palindrome"<<endl;
+    else
+    cout<<"Not palindrome"<<endl;
+}
