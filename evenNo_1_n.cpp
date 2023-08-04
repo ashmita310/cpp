67a3381dab69a36cc3cdb9f3a88af47419ff9e48
ashmita310 <ashmitasaha13@gmail.com>
2023-08-04 15:23:41 +0530

    even number printing

---
+++ evenNo_1_n.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=0,i;
+    cout<<"Enter the N:"<<endl;
+    cin>>n;
+    for(i=2;i<n;i=i+2)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
