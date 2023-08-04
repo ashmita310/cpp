9c89ce42a5881ae9ff40f18582b52dc10886380b
ashmita310 <ashmitasaha13@gmail.com>
2023-08-05 03:40:20 +0530

    star pattern with spaces

---
+++ star_pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length=0;
+    cout<<"ENTER THE LENGTH:"<<endl;
+    cin>>length;
+    for(int i=0;i<length;i++)
+    {
+        for(int j=0;j<length-i-1;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<i+1;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
